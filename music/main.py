4298b665b5e9b41e4be646af34840ebf30a1dbef
Dalton Dayton <daltondayton1@gmail.com>
2021-10-21 00:20:10 -0600

    INIT / Basic Music Bot

---
+++ music/main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import music
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN_ID = os.getenv("TOKEN_ID")
+
+cogs = [music]
+
+client = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+client.run(TOKEN_ID)
