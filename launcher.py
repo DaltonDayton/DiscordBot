3763ac9379db3be1728f4c6120b9cce0490340d5
Dalton Dayton <daltondayton1@gmail.com>
2021-10-25 23:24:09 -0600

    Restructure entire project

---
+++ launcher.py
@@ -0,0 +1,14 @@
+from bot import MusicBot
+
+# Start Lavalink server
+# Run       java -jar Lavalink.jar
+# From      [base]\jdk-13.0.2\bin
+
+
+def main():
+    bot = MusicBot()
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
