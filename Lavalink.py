24cfa835d29fe79366c9a1b01ae4166ed583036f
Dalton Dayton <daltondayton1@gmail.com>
2021-10-28 19:52:56 -0600

    add loader.py and Lavalink.py for one-click start to server

---
+++ Lavalink.py
@@ -0,0 +1,15 @@
+import os
+
+os.system(".\\jdk-13.0.2-linux\\bin\\java -jar .\\jdk-13.0.2-linux\\bin\\Lavalink.jar")
+
+# with open("data/environment.0", "r", encoding="utf-8") as f:
+#     ENVIRONMENT = f.read()
+
+# if ENVIRONMENT == "dev":
+#     print("Dev Environment")
+#     os.system(".\\jdk-13.0.2\\bin\\java.exe -jar .\\jdk-13.0.2\\bin\\Lavalink.jar")
+# elif ENVIRONMENT == "server":
+#     print("Server Environment")
+#     os.system(
+#         ".\\jdk-13.0.2-linux\\bin\\java -jar .\\jdk-13.0.2-linux\\bin\\Lavalink.jar"
+#     )
