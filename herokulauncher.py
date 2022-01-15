c9287961ce506313d88ea8241f1033e36880ab79
Dalton Dayton <daltondayton1@gmail.com>
2022-01-15 10:39:49 -0700

    add lavalink to herokulauncher

--- herokulauncher.py
+++ herokulauncher.py
@@ -4,6 +4,8 @@ from bot import MusicBot
 # Run       java -jar Lavalink.jar
 # From      [base]\jdk-13.0.2\bin
 
+os.system(".\\jdk-13.0.2\\bin\\java -jar .\\jdk-13.0.2\\bin\\Lavalink.jar")
+
 
 def main():
     bot = MusicBot()
