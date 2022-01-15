933e450dac2632325f5fb2d47a1431d72ba451ef
Dalton Dayton <daltondayton1@gmail.com>
2022-01-15 10:46:26 -0700

    update path and add wait

--- herokulauncher.py
+++ herokulauncher.py
@@ -1,12 +1,14 @@
 import os
-
+import time
 from bot import MusicBot
 
 # Start Lavalink server
 # Run       java -jar Lavalink.jar
 # From      [base]\jdk-13.0.2\bin
 
-os.system(".\\jdk-13.0.2\\bin\\java -jar .\\jdk-13.0.2\\bin\\Lavalink.jar")
+os.system("java -jar .\\jdk-13.0.2\\bin\\Lavalink.jar")
+
+time.sleep(30)
 
 
 def main():
